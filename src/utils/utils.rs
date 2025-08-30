88a83e3b70cd9692c36793143f3a5cb243b2e873
slc90 <506091625@qq.com>
2025-08-30 20:52:55 +0800

    1.增加测试例子 2.使用pretty_assertions让测试错误输出更友好

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -5,3 +5,49 @@ pub fn get_current_time_format_string() -> String {
     let now = Local::now();
     now.format("%Y-%m-%d-%H-%M-%S").to_string()
 }
+
+/// 只是用于测试的例子
+///
+/// # Arguments
+///
+/// - `left` (`u64`) - Describe this parameter.
+/// - `right` (`u64`) - Describe this parameter.
+///
+/// # Returns
+///
+/// - `u64` - Describe the return value.
+///
+/// # Examples
+///
+/// ```
+/// use crate::...;
+///
+/// let _ = add();
+/// ```
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_add_success() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_add_fail() {
+        let result = add(2, 3);
+        assert_ne!(result, 4);
+    }
+
+    #[test]
+    fn pretty_assertions_example() {
+        let result = add(2, 3);
+        assert_eq!(result, 4);
+    }
+}
